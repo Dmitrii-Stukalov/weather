facbfc858ecd1f875f3e40b4948e89607bf69dab
Dmitrii <dmitrii.stukalov@list.ru>
2021-03-19 08:21:25 +0300

    add lab3

--- app.js
+++ app.js
@@ -1,124 +1,34 @@
-const apiKey = '89b8dd844aa773508f5ca09b43fb3642';
 const placeholder = `<li>
                         <h2>Подождите, данные загружаются</h2>
                      </li>`
-let currentID = localStorage.length
-
-const degreeToDirection = deg => {
-    const val = (deg / 22.5) + .5
-    const directions = ['North', 'North-northeast', 'Northeast', 'East-northeast', 'East', 'East-southeast',
-        'Southeast', 'South-southeast', 'South', 'South-southwest', 'Southwest', 'West-southwest', 'West',
-        'West-northwest', 'Northwest', 'North-northwest']
-    return directions[Math.ceil(val) % 16]
-};
-
-const speedToType = speed => {
-    switch (true) {
-        case speed < 0.5:
-            return 'Calm'
-        case speed < 1.5:
-            return 'Light air'
-        case speed < 3.3:
-            return 'Light breeze'
-        case speed < 5.5:
-            return 'Gentle breeze'
-        case speed < 7.9:
-            return 'Moderate breeze'
-        case speed < 10.7:
-            return 'Fresh breeze'
-        case speed < 13.8:
-            return 'Strong breeze'
-        case speed < 17.1:
-            return 'Near gale'
-        case speed < 20.7:
-            return 'Gale'
-        case speed < 24.4:
-            return 'Strong gale'
-        case speed < 28.4:
-            return 'Storm'
-        case speed < 32.6:
-            return 'Violent storm'
-        default:
-            return 'Hurricane'
-    }
-}
-
-const cloudinessToType = cloudiness => {
-    switch (true) {
-        case cloudiness < 25:
-            return 'Sunny'
-        case cloudiness < 50:
-            return 'Mostly sunny'
-        case cloudiness < 69:
-            return 'Partly sunny'
-        case cloudiness < 87:
-            return 'Mostly cloudy'
-        default:
-            return 'Cloudy'
-    }
-}
-
-const weatherToIcon = type => {
-    switch (true) {
-        case type === 'Clear':
-            return 'sun-64.ico'
-        case type === 'Few clouds':
-            return 'partly-cloudy-day-64.ico'
-        case type === 'Scattered clouds' || type === 'Broken clouds' || type === 'Clouds':
-            return 'clouds-64.ico'
-        case type === 'Shower rain' || type === 'Rain':
-            return 'rain-64.ico'
-        case type === 'Thunderstorm':
-            return 'storm-64.ico'
-        case type === 'Snow':
-            return 'snow-64.ico'
-        case type === 'mist':
-            return 'dust-64.ico'
-    }
-}
 
 const fillChosen = (api) => fetch(api)
     .then(response => response.json())
     .then(data => {
-        console.log(data);
-        const {speed, deg} = data.wind;
-        const clouds = data.clouds.all;
-        const {pressure, humidity} = data.main
-        const temperature = Math.round(data.main.temp) - 273
-        const {lon, lat} = data.coord
+        document.querySelector('.wrap .chosen span').textContent = data.temperature
+        document.querySelector('.wrap .chosen h2').textContent = data.city
+        document.querySelector('.wrap .chosen img').setAttribute('src', data.icon)
 
-        document.querySelector('.wrap .chosen span').textContent = temperature + '˚C'
-        document.querySelector('.wrap .chosen h2').textContent = data.name
-        document.querySelector('.wrap .chosen img').setAttribute('src', weatherToIcon(data.weather[0].main))
-
-
-        document.querySelector('.wrap ul.info li span.wind').textContent = speedToType(speed) + ', '
-            + speed + 'm/s, ' + degreeToDirection(deg)
-        document.querySelector('.wrap ul.info li span.cloudiness').textContent = cloudinessToType(clouds)
-        document.querySelector('.wrap ul.info li span.pressure').textContent = pressure + 'hpa'
-        document.querySelector('.wrap ul.info li span.humidity').textContent = humidity + '%'
-        document.querySelector('.wrap ul.info li span.coordinates').textContent = '[' +
-            Math.round(lat * 100) / 100 + ', ' + Math.round(lon * 100) / 100 + ']'
+        document.querySelector('.wrap ul.info li span.wind').textContent = data.wind
+        document.querySelector('.wrap ul.info li span.cloudiness').textContent = data.clouds
+        document.querySelector('.wrap ul.info li span.pressure').textContent = data.pressure
+        document.querySelector('.wrap ul.info li span.humidity').textContent = data.humidity
+        document.querySelector('.wrap ul.info li span.coordinates').textContent = data.coords
 
         document.querySelector('.wrap').style.display = 'flex'
         document.querySelector('.placeholder').style.display = 'none'
-
-
     });
 
 
 const reloadChosen = () => {
     navigator.geolocation.getCurrentPosition(position => {
-        console.log(position)
         const lon = position.coords.longitude;
         const lat = position.coords.latitude;
-        const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-
-        fillChosen(api)
+        const local = `http://localhost:3000/weather/coordinates?lat=${lat}&long=${lon}`
+        fillChosen(local)
 
     }, _ => {
-        console.log('Denied')
-        fillChosen(`https://api.openweathermap.org/data/2.5/weather?q=london&appid=${apiKey}`)
+        fillChosen(`http://localhost:3000/weather/city?q=london`)
     })
 }
 
@@ -129,53 +39,39 @@ document.querySelector('header button.mobile').addEventListener('click', reloadC
 
 const deleteCity = button => {
     const city = button.parentNode.childNodes[1].textContent
-    for (let i = 0; i < localStorage.length; i++) {
-        let key = localStorage.key(i);
-        if (localStorage.getItem(key) === city) {
-            localStorage.removeItem(key)
-            currentID--;
-        }
-    }
-    const ul = button.parentNode.parentNode.parentNode
-    ul.removeChild(button.parentNode.parentNode)
+    fetch('http://localhost:3000/favourites', {
+        method: 'delete',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({name: city})
+    }).then(() => {
+        const ul = button.parentNode.parentNode.parentNode
+        ul.removeChild(button.parentNode.parentNode)
+    }).catch(er => console.log(er))
 }
 
 const generateCityLi = city => {
-    const api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
-    return fetch(api)
-        .then(response => response.ok ? response.json() : Promise.reject('City not found')
-        )
+    const local = `http://localhost:3000/weather/city?q=${city}`
+    return fetch(local)
+        .then(response => response.ok ? response.json() : Promise.reject('City not found'))
         .then(data => {
-            const {speed, deg} = data.wind;
-            const clouds = data.clouds.all;
-            const {pressure, humidity} = data.main
-            const temperature = Math.round(data.main.temp) - 273
-            const {lon, lat} = data.coord
-
-            const tempInfo = temperature + '˚C'
-            const icon = weatherToIcon(data.weather[0].main)
-
-            const windInfo = speedToType(speed) + ', ' + speed + 'm/s, ' + degreeToDirection(deg)
-            const cloudsInfo = cloudinessToType(clouds)
-            const pressureInfo = pressure + 'hpa'
-            const humidityInfo = humidity + '%'
-            const coordsInfo = '[' + Math.round(lat * 100) / 100 + ', ' + Math.round(lon * 100) / 100 + ']'
-
+            console.log(data)
             return `<li>
                 <div class="city">
-                    <h3>${city}</h3>
-                    <span>${tempInfo}</span>
-                    <img alt="weather icon" src="${icon}">
+                    <h3>${data.city}</h3>
+                    <span>${data.temperature}</span>
+                    <img alt="weather icon" src="${data.icon}">
                     <button onclick="deleteCity(this)">x</button>
                 </div>
                 <ul class="info">
-                    <li>Ветер <span>${windInfo}</span></li>
-                    <li>Облачность <span>${cloudsInfo}</span></li>
-                    <li>Давление <span>${pressureInfo}</span></li>
-                    <li>Влажность <span>${humidityInfo}</span></li>
-                    <li>Координаты <span>${coordsInfo}</span></li>
+                    <li>Ветер <span>${data.wind}</span></li>
+                    <li>Облачность <span>${data.clouds}</span></li>
+                    <li>Давление <span>${data.pressure}</span></li>
+                    <li>Влажность <span>${data.humidity}</span></li>
+                    <li>Координаты <span>${data.coords}</span></li>
                 </ul>
-            </li>`
+            </li>`;
         })
 }
 
@@ -202,31 +98,38 @@ const onSubmit = event => {
     addCity(city, true)
 }
 
-const addCity = async (city, addToStorage = false) => {
+const addCity = (city, addToStorage = false) => {
     const ul = document.querySelector('.favourite ul')
     ul.insertAdjacentHTML('beforeend', generateCityPlaceholder(city))
-    await generateCityLi(city).then(li => {
-        ul.lastChild.style.display = 'none'
-        ul.insertAdjacentHTML('beforeend', li)
+    generateCityLi(city).then(li => {
         if (addToStorage) {
-            localStorage.setItem(currentID++, city)
+            fetch('http://localhost:3000/favourites', {
+                method: 'post',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({name: city})
+            })
+                .then(() => {
+                    ul.lastChild.style.display = 'none'
+                    ul.insertAdjacentHTML('beforeend', li)
+                })
+        } else {
+            ul.lastChild.style.display = 'none'
+            ul.insertAdjacentHTML('beforeend', li)
         }
-        console.log(localStorage)
     }).catch(err => {
         ul.lastChild.style.display = 'none'
         alert(err)
     })
 }
 
-const addAllCities = async () => {
-    for (let i = 0; i < localStorage.length; i++) {
-        let key = localStorage.key(i);
-        await addCity(localStorage.getItem(key));
-    }
+const addAllCities = () => {
+    fetch('http://localhost:3000/favourites')
+        .then(response => response.json())
+        .then(cities => cities.forEach(city => addCity(city)))
 }
 
 window.addEventListener('load', addAllCities);
 
 document.forms[0].addEventListener('submit', onSubmit)
-
-
---
+++ index.js
@@ -0,0 +1,177 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const apiKey = '89b8dd844aa773508f5ca09b43fb3642';
+const fetch = require('node-fetch')
+
+const PORT = process.env.PORT || 3000
+
+const app = express()
+app.use(cors())
+app.use(require('body-parser').json())
+
+const schema = new mongoose.Schema({name: {type: String, required: true}})
+const City = mongoose.model('City', schema)
+
+async function start() {
+    try {
+        await mongoose.connect(
+            'mongodb+srv://dima:weblab3@cluster0.8ohck.mongodb.net/weather',
+            {
+                useNewUrlParser: true,
+                useFindAndModify: false,
+                useUnifiedTopology: true
+            }
+        )
+        app.listen(PORT, () => {
+            console.log('Server has been started...')
+        })
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+const degreeToDirection = deg => {
+    const val = (deg / 22.5) + .5
+    const directions = ['North', 'North-northeast', 'Northeast', 'East-northeast', 'East', 'East-southeast',
+        'Southeast', 'South-southeast', 'South', 'South-southwest', 'Southwest', 'West-southwest', 'West',
+        'West-northwest', 'Northwest', 'North-northwest']
+    return directions[Math.ceil(val) % 16]
+};
+
+const speedToType = speed => {
+    switch (true) {
+        case speed < 0.5:
+            return 'Calm'
+        case speed < 1.5:
+            return 'Light air'
+        case speed < 3.3:
+            return 'Light breeze'
+        case speed < 5.5:
+            return 'Gentle breeze'
+        case speed < 7.9:
+            return 'Moderate breeze'
+        case speed < 10.7:
+            return 'Fresh breeze'
+        case speed < 13.8:
+            return 'Strong breeze'
+        case speed < 17.1:
+            return 'Near gale'
+        case speed < 20.7:
+            return 'Gale'
+        case speed < 24.4:
+            return 'Strong gale'
+        case speed < 28.4:
+            return 'Storm'
+        case speed < 32.6:
+            return 'Violent storm'
+        default:
+            return 'Hurricane'
+    }
+}
+
+const cloudinessToType = cloudiness => {
+    switch (true) {
+        case cloudiness < 25:
+            return 'Sunny'
+        case cloudiness < 50:
+            return 'Mostly sunny'
+        case cloudiness < 69:
+            return 'Partly sunny'
+        case cloudiness < 87:
+            return 'Mostly cloudy'
+        default:
+            return 'Cloudy'
+    }
+}
+
+const weatherToIcon = type => {
+    switch (true) {
+        case type === 'Clear':
+            return 'sun-64.ico'
+        case type === 'Few clouds':
+            return 'partly-cloudy-day-64.ico'
+        case type === 'Scattered clouds' || type === 'Broken clouds' || type === 'Clouds':
+            return 'clouds-64.ico'
+        case type === 'Shower rain' || type === 'Rain':
+            return 'rain-64.ico'
+        case type === 'Thunderstorm':
+            return 'storm-64.ico'
+        case type === 'Snow':
+            return 'snow-64.ico'
+        case type === 'mist':
+            return 'dust-64.ico'
+    }
+}
+
+const processData = data => {
+    const {speed, deg} = data.wind;
+    const clouds = data.clouds.all;
+    const {pressure, humidity} = data.main
+    const temperature = Math.round(data.main.temp) - 273
+    const {lon, lat} = data.coord
+
+    const tempInfo = temperature + '˚C'
+    const icon = weatherToIcon(data.weather[0].main)
+
+    const windInfo = speedToType(speed) + ', ' + speed + 'm/s, ' + degreeToDirection(deg)
+    const cloudsInfo = cloudinessToType(clouds)
+    const pressureInfo = pressure + 'hpa'
+    const humidityInfo = humidity + '%'
+    const coordsInfo = '[' + Math.round(lat * 100) / 100 + ', ' + Math.round(lon * 100) / 100 + ']'
+
+    return {
+        city: data.name,
+        temperature: tempInfo,
+        icon: icon,
+        wind: windInfo,
+        clouds: cloudsInfo,
+        pressure: pressureInfo,
+        humidity: humidityInfo,
+        coords: coordsInfo
+    }
+}
+
+
+app.get('/weather/city', (req, res) => {
+    const city = req.query.q
+    const api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+    fetch(api)
+        .then(response => response.ok ? response.json() : Promise.reject('City not found'))
+        .then(data => res.status(200).send(processData(data)))
+        .catch(er => res.status(404).send(er))
+})
+
+app.get('/weather/coordinates', (req, res) => {
+    const lon = req.query.long
+    const lat = req.query.lat
+    const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`
+    fetch(api)
+        .then(response => response.ok ? response.json() : Promise.reject('City not found'))
+        .then(data => {
+            res.status(200).send(processData(data))
+        })
+        .catch(er => res.status(404).send(er))
+})
+
+app.get('/favourites', (req, res) => {
+    City.find({}, (err, cities) => {
+        const names = Array()
+        cities.forEach(city => names.push(city.name))
+        res.status(200).send(names)
+    })
+})
+
+app.post('/favourites', (req, res) => {
+    City({name: req.body.name}).save()
+        .then(res.status(200).send())
+        .catch(er => res.status(500).send(er))
+})
+
+app.delete('/favourites', (req, res) => {
+    City.deleteOne({name: req.body.name})
+        .then(res.status(200).send())
+        .catch(er => res.status(500).send(er))
+})
+
+start()
